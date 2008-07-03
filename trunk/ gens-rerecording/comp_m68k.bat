21bd315a41f14586dece14bef0882eee925ea9ae
Upthorn <Upthorn@e27e91e0-2050-0410-b00d-0de3e104d0ab>
2008-07-03 00:52:28 +0000

    mostly just getting stuff to compile 	Jun 22, 2008 	nitsu...@hotmail.com

--- trunk/ gens-rerecording/comp_m68k.bat
+++ trunk/ gens-rerecording/comp_m68k.bat
@@ -2,5 +2,5 @@ rem Main 68000 compilation (Main68k\star.c has been compiled before)
 
 @cd starscream\main68k\release
 star temp.asm -hog -name main68k_
-nasm\nasmw -f win32 temp.asm -o ..\..\..\gens\release\main68k.obj
+nasm\nasmw -f win32 temp.asm -o ..\..\..\gens\libs\main68k.obj
 @cd..\..\..
