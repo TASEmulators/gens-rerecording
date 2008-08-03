90714463e25fe0539d849d110ef5bce6e5ba0995
andres.delikat <andres.delikat@e27e91e0-2050-0410-b00d-0de3e104d0ab>
2008-08-03 15:48:46 +0000

    Ramwatch moved to ramwatch.cpp

---  gens-rerecording/Gens/G_main.h
+++  gens-rerecording/Gens/G_main.h
@@ -100,6 +100,6 @@ void Update_RAM_Search();
 void Update_RAM_Cheats();
 int SaveFlags();
 void LoadFlags(int flags);
-
+void init_list_box(HWND Box, char Strs[][11], int numColumns, int *columnWidths);
 
 #endif
