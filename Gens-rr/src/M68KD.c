f5013146389255d4d7bc27e5ce5bafd93c318d4e
realmonster <r57shell@uralweb.ru>
2017-09-04 17:29:22 +0500

    M68k disasm. Invalid Opcodes

--- Gens-rr/src/M68KD.c
+++ Gens-rr/src/M68KD.c
@@ -264,10 +264,14 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 			}
 			else
 			{
+				if ((OPC & 0x3F) >= 0x3A && ((OPC >> 6) & 0x3) != 0)
+					break; // invalid d16(PC), d8(PC,ix), #imm except BTST
 				switch((OPC >> 6) & 0x3)
 				{
 					case 0:
 						//BTST  Ds,a
+						if ((OPC & 0x3F) > 0x3C)
+							break; // invalid <ea>
 						sprintf(Dbg_Str, "BTST    D%.1d,%s", (OPC & 0xE00) >> 9, Make_Dbg_EA_Str(0, (OPC & 0x38) >> 3, OPC & 7));
 						break;
 
@@ -346,6 +350,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 		}
 		else
 		{
+			if ((OPC & 0x38) == 0x08) // none of them supports Ar mode
+				break;
 			switch((OPC >> 6) & 0x3F)
 			{
 				case 0:
@@ -485,18 +491,29 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 1:
 			//MOVE.b  as,ad
+			if ((OPC & 0x38) == 8 || (OPC & 0x1C0) == 0x40) // source or destination can't be Ar
+				break;
+			if ((OPC & 0x3F) > 0x3C // invalid source <ea>
+			|| ((OPC & 0x1C0) == 0x1C0 && (OPC & 0xC00))) // destination can't be PC-based or #imm
+				break;
 			sprintf(Tmp_Str, "%s", Make_Dbg_EA_Str(0, (OPC >> 3) & 0x7, OPC & 0x7));
 			sprintf(Dbg_Str, "MOVE.B  %s,%s", Tmp_Str, Make_Dbg_EA_Str(0, (OPC >> 6) & 0x7, (OPC >> 9) & 0x7));
 			break;
 
 		case 2:
 			//MOVE.l  as,ad
+			if ((OPC & 0x3F) > 0x3C // invalid source <ea>
+			|| ((OPC & 0x1C0) == 0x1C0 && (OPC & 0xC00))) // destination can't be PC-based or #imm
+				break;
 			sprintf(Tmp_Str, "%s", Make_Dbg_EA_Str(2, (OPC >> 3) & 0x7, OPC & 0x7));
 			sprintf(Dbg_Str, "MOVE%s %s,%s", ((OPC & 0x1C0) == 0x40 ? "A.L" : ".L "), Tmp_Str, Make_Dbg_EA_Str(2, (OPC >> 6) & 0x7, (OPC >> 9) & 0x7));
 			break;
 
 		case 3:
 			//MOVE.w  as,ad
+			if ((OPC & 0x3F) > 0x3C // invalid source <ea>
+			|| ((OPC & 0x1C0) == 0x1C0 && (OPC & 0xC00))) // destination can't be PC-based or #imm
+				break;
 			sprintf(Tmp_Str, "%s", Make_Dbg_EA_Str(1, (OPC >> 3) & 0x7, OPC & 0x7));
 			sprintf(Dbg_Str, "MOVE%s %s,%s", ((OPC & 0x1C0) == 0x40 ? "A.W" : ".W "), Tmp_Str, Make_Dbg_EA_Str(1, (OPC >> 6) & 0x7, (OPC >> 9) & 0x7));
 			break;
@@ -504,17 +521,30 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 		case 4:
 			//SPECIALS ...
 
+			if ((OPC & 0x3F) > 0x3C) // invalid <ea> or opcode
+				break;
+			if ((OPC & 0x38) == 8 && (OPC & 0xFFF8) != 0x4E48) // Ar is illegal except TRAP
+				break;
+
 			if (OPC & 0x100)
 			{
+				if (!(OPC & 0x80))
+					break;
 				if (OPC & 0x40)
+				{
 					//LEA  a,Ad
+					if ((OPC & 0x38) == 0 || (OPC & 0x38) == 0x18 || (OPC & 0x38) == 0x20 || (OPC & 0x3F) == 0x3C)
+						break; // illegal Dr, (Ar)+, -(Ar), #imm
 					sprintf(Dbg_Str, "LEA     %s,A%.1d", Make_Dbg_EA_Str(2, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
+				}
 				else
 					//CHK.W  a,Dd
 					sprintf(Dbg_Str, "CHK.W   %s,D%.1d", Make_Dbg_EA_Str(1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
 			}
 			else
 			{
+				if (OPC < 0x4C80 && (OPC & 0x3F) >= 0x3A && (OPC & 0xC0) != 0xC0 && (OPC & 0xFC0) != 0x840)
+					break; // illegal d16(PC), d8(PC,xn), #imm
 				switch((OPC >> 6) & 0x3F)
 				{
 					case 0:	case 1: case 2:
@@ -524,6 +554,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 					case 3:
 						//MOVE  SR,a
+						if ((OPC & 0x3F) >= 0x3A)
+							break; // illegal d16(PC), d8(PC,xn), #imm
 						sprintf(Dbg_Str, "MOVE    SR,%s", Make_Dbg_EA_Str(1, (OPC & 0x38) >> 3, OPC & 0x7));
 						break;
 
@@ -560,23 +592,32 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					case 33:
 
 						if (OPC & 0x38)
+						{
 							//PEA  a
+							if ((OPC & 0x38) == 0x18 || (OPC & 0x38) == 0x20 || (OPC & 0x3F) == 0x3C)
+								break; // illegal (Ar)+, -(Ar), #imm
 							sprintf(Dbg_Str, "PEA     %s", Make_Dbg_EA_Str(2, (OPC & 0x38) >> 3, OPC & 0x7));
+						}
 						else
 							//SWAP.w  Dd
 							sprintf(Dbg_Str, "SWAP.W  D%d", OPC & 0x7);
 
 						break;
 
-					case 34: case 35:
+					case 35:
+						if ((OPC & 0x3F) >= 0x3A)
+							break; // illegal d16(PC), d8(PC,ix), #imm
+					case 34:
 
 						if (OPC & 0x38)
 						{
-							int registers_a7_d0 = Next_Word();
+							if ((OPC & 0x38) == 0x18)
+								break; // illegal (Ar)+
+							i = Next_Word(); // Reg-List
 
 							//MOVEM.z Reg-List,a
 							sprintf(Dbg_Str, "MOVEM%-3s{d0-a7}[%02x %02x],%s", Make_Dbg_Size_Str_2((OPC >> 6) & 1),
-								registers_a7_d0 >> 8, registers_a7_d0 & 0xff,
+								i >> 8, i & 0xff,
 								Make_Dbg_EA_Str_2((OPC >> 6) & 1, (OPC & 0x38) >> 3, OPC & 0x7));
 						}
 						else
@@ -598,6 +639,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 							break;
 						}
 						//TAS.b a
+						if ((OPC & 0x3F) >= 0x3A)
+							break; // illegal d16(PC), d8(PC,xn), #imm
 						sprintf(Dbg_Str, "TAS.B   %s", Make_Dbg_EA_Str(0, (OPC & 0x38) >> 3, OPC & 0x7));
 						break;
 
@@ -608,11 +651,13 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 					case 50: case 51:
 						{
-						int registers_d0_a7 = Next_Word();
+						if ((OPC & 0x38) == 0 || (OPC & 0x38) == 0x20 || (OPC & 0x3F) == 0x3C)
+							break; // illegal Dr, -(Ar), #imm
+						i = Next_Word(); // Reg-List
 
 						//MOVEM.z a,Reg-List
 						sprintf(Dbg_Str, "MOVEM%-3s%s,{a7-d0}[%02x %02x]", Make_Dbg_Size_Str_2((OPC >> 6) & 1), Make_Dbg_EA_Str_2((OPC >> 6) & 1, (OPC & 0x38) >> 3, OPC & 0x7),
-							registers_d0_a7 >> 8, registers_d0_a7 & 0xff);
+							i >> 8, i & 0xff);
 						}
 						break;
 
@@ -695,11 +740,15 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 					case 58:
 						//JSR  a
+						if ((OPC & 0x38) == 0 || (OPC & 0x38) == 0x18 || (OPC & 0x38) == 0x20 || (OPC & 0x3F) == 0x3C)
+							break; // illegal Dr, (Ar)+, -(Ar), #imm
 						sprintf(Dbg_Str, "JSR     %s", Make_Dbg_EA_Str(2, (OPC & 0x38) >> 3, OPC & 0x7));
 						break;
 
 					case 59:
 						//JMP  a
+						if ((OPC & 0x38) == 0 || (OPC & 0x38) == 0x18 || (OPC & 0x38) == 0x20 || (OPC & 0x3F) == 0x3C)
+							break; // illegal Dr, (Ar)+, -(Ar), #imm
 						sprintf(Dbg_Str, "JMP     %s", Make_Dbg_EA_Str(2, (OPC & 0x38) >> 3, OPC & 0x7));
 						break;
 				}
@@ -724,12 +773,18 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 				else
 				{
 					//SCC.b  a
+					if ((OPC & 0x3F) >= 0x3A || (OPC & 0x38) == 8)
+						break; // illegal Ar, d16(PC), d8(PC,ix), #imm
 					sprintf(Dbg_Str, "S%-7s%s", Make_Dbg_Cond_Str((OPC >> 8) & 0xF), Make_Dbg_EA_Str(0, (OPC & 0x38) >> 3, OPC & 0x7));
 				}
 				break;
 			}
 			else
 			{
+				if ((OPC & 0x3F) >= 0x3A)
+					break; // illegal d16(PC), d8(PC,ix), #imm
+				if ((OPC & 0x38) == 8 && (OPC & 0xC0) == 0)
+					break; // illegal Ar for byte
 				if (OPC & 0x100)
 					//SUBQ.z  #k3,a
 					sprintf(Dbg_Str, "SUBQ%-4s#%.1d,%s", Make_Dbg_Size_Str((OPC >> 6) & 3), ((OPC >> 9) & 0x7)?(OPC >> 9) & 0x7:8, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
@@ -794,11 +849,15 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 7:
 			//MOVEQ  #k8,Dd
+			if (OPC & 0x100)
+				break;
 			sprintf(Dbg_Str, "MOVEQ   #$%.2X,D%.1d", OPC & 0xFF, (OPC >> 9) & 0x7);
 			break;
 
 		case 8:
 
+			if ((OPC & 0x3F) > 0x3C)
+				break; // invalid <ea>
 			if (OPC & 0x100)
   			{
 				if (!(OPC & 0xF8))
@@ -815,15 +874,19 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					break;
 				}
 
+				if ((OPC & 0x38) == 8)
+					break; // illegal Ar
 				if ((OPC & 0xC0) == 0xC0)
 					//DIVS.w  a,Dd
 					sprintf(Dbg_Str, "DIVS.W  %s,D%.1d", Make_Dbg_EA_Str(1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
-				else
+				else if ((OPC & 0x3F) < 0x3A && (OPC & 0x38)) // illegal Dr, d16(PC), d8(PC,ix), #imm
 					//OR.z  Ds,a
 					sprintf(Dbg_Str, "OR%-6sD%.1d;%s", Make_Dbg_Size_Str((OPC >> 6) & 3), (OPC >> 9) & 0x7, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
 			}
 			else
 			{
+				if ((OPC & 0x38) == 8)
+					break; // illegal Ar
 				if ((OPC & 0xC0) == 0xC0)
 					//DIVU.w  a,Dd
 					sprintf(Dbg_Str, "DIVU.W  %s,D%.1d", Make_Dbg_EA_Str(1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
@@ -835,6 +898,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 9:
 
+			if ((OPC & 0x3F) > 0x3C)
+				break; // invalid <ea>
 			if ((OPC & 0xC0) == 0xC0)
 				//SUBA.z  a,Ad
 				sprintf(Dbg_Str, "SUBA%-4s%s,A%.1d", Make_Dbg_Size_Str_2((OPC >> 8) & 1), Make_Dbg_EA_Str_2((OPC >> 8) & 1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
@@ -857,9 +922,11 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					}
 
 					//SUB.z  Ds,a
+					if ((OPC & 0x3F) >= 0x3A)
+						break; // illegal d16(PC), d8(PC,ix), #imm
 					sprintf(Dbg_Str, "SUB%-5sD%.1d,%s", Make_Dbg_Size_Str((OPC >> 6) & 0x3), (OPC >> 9) & 0x7, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
 				}
-				else
+				else if ((OPC & 0x38) != 8 || (OPC & 0xC0)) // illegal Ar for byte size
 					//SUB.z  a,Dd
 					sprintf(Dbg_Str, "SUB%-5s%s,D%.1d", Make_Dbg_Size_Str((OPC >> 6) & 0x3), Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
 			}
@@ -872,6 +939,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 11:
 
+			if ((OPC & 0x3F) > 0x3C)
+				break; // invalid <ea>
 			if ((OPC & 0xC0) == 0xC0)
 				//CMPA.z  a,Ad
 				sprintf(Dbg_Str, "CMPA%-4s%s,A%.1d", Make_Dbg_Size_Str_2((OPC >> 8) & 1), Make_Dbg_EA_Str_2((OPC >> 8) & 1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
@@ -887,9 +956,11 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					}
 
 					//EOR.z  Ds,a
+					if ((OPC & 0x3F) >= 0x3A)
+						break; // illegal d16(PC), d8(PC,ix), #imm
 					sprintf(Dbg_Str, "EOR%-5sD%.1d,%s", Make_Dbg_Size_Str((OPC >> 6) & 0x3), (OPC >> 9) & 0x7, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
 				}
-				else
+				else if ((OPC & 0x38) != 8 || (OPC & 0xC0)) // illegal Ar for byte size
 					//CMP.z  a,Dd
 					sprintf(Dbg_Str, "CMP%-5s%s,D%.1d", Make_Dbg_Size_Str((OPC >> 6) & 0x3), Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
 			}
@@ -897,38 +968,30 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 12:
 
-			if ((OPC & 0X1F8) == 0x100)
+			if ((OPC & 0x3F) > 0x3C)
+				break; // invalid <ea>
+			if ((OPC & 0x38) == 0x8)
 			{
-				//ABCD Ds,Dd
-				sprintf(Dbg_Str, "ABCD    D%.1d,D%.1d", OPC & 0x7, (OPC >> 9) & 0x7);
-				break;
-			}
+				if ((OPC & 0X1C0) == 0x100)
+				{
+					//ABCD -(As),-(Ad)
+					sprintf(Dbg_Str, "ABCD    -(A%.1d),-(A%.1d)", OPC & 0x7, (OPC >> 9) & 0x7);
+					break;
+				}
 
-			if ((OPC & 0X1F8) == 0x140)
-			{
-				//EXG.l Ds,Dd
-				sprintf(Dbg_Str, "EXG.L   D%.1d,D%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
-				break;
-			}
+				if ((OPC & 0X1C0) == 0x140)
+				{
+					//EXG.l As,Ad
+					sprintf(Dbg_Str, "EXG.L   A%.1d,A%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
+					break;
+				}
 
-			if ((OPC & 0X1F8) == 0x108)
-			{
-				//ABCD -(As),-(Ad)
-				sprintf(Dbg_Str, "ABCD    -(A%.1d),-(A%.1d)", OPC & 0x7, (OPC >> 9) & 0x7);
-				break;
-			}
-
-			if ((OPC & 0X1F8) == 0x148)
-			{
-				//EXG.l As,Ad
-				sprintf(Dbg_Str, "EXG.L   A%.1d,A%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
-				break;
-			}
-
-			if ((OPC & 0X1F8) == 0x188)
-			{
-				//EXG.l Ds,Ad
-				sprintf(Dbg_Str, "EXG.L   D%.1d,A%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
+				if ((OPC & 0X1C0) == 0x180)
+				{
+					//EXG.l Ds,Ad
+					sprintf(Dbg_Str, "EXG.L   D%.1d,A%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
+					break;
+				}
 				break;
 			}
 
@@ -944,8 +1007,31 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					sprintf(Dbg_Str, "MULU.W  %s,D%.1d", Make_Dbg_EA_Str(1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
 					break;
 
-				case 4: case 5: case 6:
+				case 4: //AND.b
+					if (!(OPC & 0x38))
+					{
+						//ABCD Ds,Dd
+						sprintf(Dbg_Str, "ABCD    D%.1d,D%.1d", OPC & 0x7, (OPC >> 9) & 0x7);
+						break;
+					}
+					goto andz;
+
+				case 5: //AND.w
+					if (!(OPC & 0x38))
+					{
+						//EXG.l Ds,Dd
+						sprintf(Dbg_Str, "EXG.L   D%.1d,D%.1d", (OPC >> 9) & 0x7, OPC & 0x7);
+						break;
+					}
+					goto andz;
+
+				case 6: //AND.l
+					if (!(OPC & 0x38))
+						break;
+				andz:
 					//AND.z  Ds,a
+					if ((OPC & 0x3F) >= 0x3A)
+						break; // illegal d16(PC), d8(PC,ix), #imm
 					sprintf(Dbg_Str, "AND%-5sD%.1d,%s", Make_Dbg_Size_Str((OPC >> 6) & 0x3), (OPC >> 9) & 0x7, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
 					break;
 
@@ -958,6 +1044,8 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 		case 13:
 
+			if ((OPC & 0x3F) > 0x3C)
+				break; // invalid <ea>
 			if ((OPC & 0xC0) == 0xC0)
 				//ADDA.z  a,Ad
 				sprintf(Dbg_Str, "ADDA%-4s%s,A%.1d", Make_Dbg_Size_Str_2((OPC >> 8) & 1), Make_Dbg_EA_Str_2((OPC >> 8) & 1, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
@@ -980,9 +1068,11 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 					}
 
 					//ADD.z  Ds,a
+					if ((OPC & 0x3F) >= 0x3A)
+						break; // illegal d16(PC), d8(PC,ix), #imm
 					sprintf(Dbg_Str, "ADD%-5sD%.1d,%s", Make_Dbg_Size_Str((OPC >> 6) & 0x3), (OPC >> 9) & 0x7, Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7));
 				}
-				else
+				else if ((OPC & 0x38) != 8 || (OPC & 0xC0)) // illegal Ar for byte size
 					//ADD.z  a,Dd
 					sprintf(Dbg_Str, "ADD%-5s%s,D%.1d", Make_Dbg_Size_Str((OPC >> 6) & 0x3), Make_Dbg_EA_Str((OPC >> 6) & 3, (OPC & 0x38) >> 3, OPC & 0x7), (OPC >> 9) & 0x7);
 			}
@@ -992,6 +1082,10 @@ char *M68KDisasm_(unsigned short (*NW)(), unsigned int (*NL)(), int hook, unsign
 
 			if ((OPC & 0xC0) == 0xC0)
 			{
+				if ((OPC & 0x3F) < 0x10 || (OPC & 0x3F) >= 0x3A)
+					break; // illegal Dr, Ar, d16(PC), d8(PC,ix), #imm
+				if (OPC & 0x800)
+					break;
 				switch ((OPC >> 8) & 0x7)
 				{
 					case 0:
